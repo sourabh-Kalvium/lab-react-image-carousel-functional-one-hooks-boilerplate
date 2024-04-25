31e73eafc726a708d7b441fdfef0ee0cc5d142a9
sourabhkt1997 <sourabh.dhanarajan@gmail.com>
2024-04-25 09:31:38 +0530

    image carousal functional

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,40 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+
+    
+    const [count,setCount]= useState(0)
+
+    const showNext=()=>{
+        if(count<images.length-1){
+        setCount(count+1)
+        }
+    }
+
+    const showPrevious=()=>{
+        if(count>0){
+        setCount(count-1)
+        }
+    }
+    // function showPrevious() {
+    //     setCount(count - 1);
+    // }
+   
+
+    return(
+    <div className="main">
+        <div className="arrow"><ArrowBackIosIcon onClick={showPrevious} className="arrow-button" /></div>
+
+        <div className="face-container" style={{ backgroundImage: `url(${images[count].img})` }}>
+
+            <h1>{images[count].title}</h1>
+            <p>{images[count].subtitle}</p>
+
+        </div>
+
+        <div className="arrow"><ArrowForwardIosIcon onClick={showNext} className="arrow-button" /></div>
+    </div>
+    )
 }
 
 export default Carousel;
